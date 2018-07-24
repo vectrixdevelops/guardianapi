5729db8f29c256a838219b847817cae25ba2ffe4
Connor Hartley <vectrixu+gh@gmail.com>
2018-07-24 17:37:12 +1200

    Fix compile errors

--- src/main/java/com/ichorpowered/guardian/api/detection/stage/StageProcess.java
+++ src/main/java/com/ichorpowered/guardian/api/detection/stage/StageProcess.java
@@ -1,7 +1,9 @@
 /*
  * MIT License
  *
- * Copyright (c) 2017 Connor Hartley
+ * Copyright (c) 2018 Connor Hartley
+ * Copyright (c) 2018 SpongePowered
+ * Copyright (c) 2018 contributors
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -10,8 +12,8 @@
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -30,6 +32,6 @@ import org.checkerframework.checker.nullness.qual.NonNull;
  */
 public interface StageProcess {
 
-    @NonNull Class<? extends Stage> getStageType();
+    @NonNull Class<? extends Stage<?>> getStageType();
 
 }
